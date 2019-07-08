6d9c4128972680322b8a431d2cdba5331574cf89
energiatel <energiatel@gmail.com>
2019-07-09 00:28:50 +0200

    Added file_containing_juicy_info. Need to filter research by results or not

--- dorks.sh
+++ dorks.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-google_dorks
+google_dorks_file_containing_juicy_info()
 {
-	echo "Google dorks for $1"
+	while read dork;
+	do
+		echo "https://www.google.com/search?q=site:$1 $dork"
+	done < /root/common_files/dorks/file_containing_juicy_info
+}
+
+google_dorks_open_redirects()
+{
+	echo "Google dorks for open redirects on $1"
+	echo "Usage: ./$0 google_dorks_open_redirects domain.com"
 	echo "https://www.google.com/search?q=site:http://docs.google.com docs.google.com $1"
 }
 
-shodan
+shodan()
 {
+	echo "shodan"
 }
 
-github
+github()
 {
-	#usare https://github.com/techgaun/github-dorks
-	#modificare https://github.com/energiatel/altdns/blob/master/Dockerfile per dockerizzare
+	echo "github"
+	#docker run -e GH_USER='energiatel@gmail.com' -e GH_PWD='fifa99FIFA))' -e GH_TOKEN='a8c06aab5392e76f557e85878be44d54be82d432' githubdorks2 -u myetherwallet
 }
 
-pastebin
+pastebin()
 {
+	echo "pastebin"
 }
 
-trello
+trello()
 {
+	echo "trello"
 }
+
+google_dorks_file_containing_juicy_info $1
